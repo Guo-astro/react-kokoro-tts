87e50ba6cbef0c860d53b4d621c5aeb0c6ea56f3
Guo <guo.yansong@optimind.tech>
2025-01-18 19:01:36 +0900

    first commit

---
+++ src/example/App.tsx
@@ -0,0 +1,13 @@
+import { KokoroComponent } from "@/lib/main";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col bg-white dark:bg-gray-800 transition-colors duration-300">
+      <Header />
+      <KokoroComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { KokoroComponent } from "@/lib/main";
